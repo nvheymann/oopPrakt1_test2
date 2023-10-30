1f2fb44cadd57946376b333f303d08e06c440be9
Nico von Heymann <nico@agentur-pottkinder.de>
2023-10-30 11:14:32 +0100

    chore: init

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import gui.FreizeitbaederAnwendersystem;
+import gui.FreizeitbaederControl;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		new FreizeitbaederAnwendersystem(primaryStage);
+	}	
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+}
